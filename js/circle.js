f57b41ae754f70f5428272e2186f573d6ddb66b4
Alexandra <fossum.alexandra@gmail.com>
2019-04-15 09:03:06 -0500

    few tweaks

--- js/circle.js
+++ js/circle.js
@@ -5,12 +5,11 @@
     var circle = {
         radius: 3,
 
-        getArea: function (radius) {
+        getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-            var area = Math.PI * Math.pow(this.radius, 2);
 
-            return area; // TODO: return the proper value
+            return Math.PI * Math.pow(this.radius, 2); // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
@@ -22,6 +21,8 @@
 
             if (doRounding) {
                 area = Math.round(this.getArea());
+            } else {
+                area = this.getArea();
             }
 
             console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
