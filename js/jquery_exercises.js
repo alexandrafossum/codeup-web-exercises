c697217f78b4a962659e9f8b27cfc0a77067b902
Alexandra <fossum.alexandra@gmail.com>
2019-04-23 10:57:41 -0500

    first commit

---
+++ js/jquery_exercises.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// $(document).ready(function() {
+//     alert("I'm done!");
+// });
+
+
+
+//EXERCISE STUFF
+
+alert($("h1").html());
+
+alert($("#alert").html());
+
+$("li").css({"font-weight": "20px",
+                    "color": "red",
+                    "list-style": "none"});
+
+$(".codeup").css("border", "red 1px solid");
+
+$("button").click(function() {
+    alert("We are now no longer the Knights Who Say Ni!");
+});
+
+$("h1, p, li").css({"background-color": "lightgrey"});
+
+
+
+
