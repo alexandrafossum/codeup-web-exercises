df2e264ee13dff1783c4226ea6ac7ca58820f07d
Alexandra <fossum.alexandra@gmail.com>
2019-04-23 12:06:14 -0500

    commit before event exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -6,23 +6,28 @@
 
 
 
-//EXERCISE STUFF
+// alert($("h1").html());
+//
+// alert($("#alert").html());
+//
+// $("li").css({"font-weight": "20px",
+//                     "color": "red",
+//                     "list-style": "none"});
+//
+// $(".codeup").css("border", "red 1px solid");
+//
+// $("button").click(function() {
+//     alert("We are now no longer the Knights Who Say Ni!");
+// });
+//
+// $("h1, p, li").css({"background-color": "lightgrey"});
+//
+// $("li").css("font-size", "20px");
+//
+// alert($("h1").text());
 
-alert($("h1").html());
 
-alert($("#alert").html());
 
-$("li").css({"font-weight": "20px",
-                    "color": "red",
-                    "list-style": "none"});
-
-$(".codeup").css("border", "red 1px solid");
-
-$("button").click(function() {
-    alert("We are now no longer the Knights Who Say Ni!");
-});
-
-$("h1, p, li").css({"background-color": "lightgrey"});
 
 
 
