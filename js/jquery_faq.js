f91499bdaa59d44a244ac2836e5054f96e241f8a
Alexandra <fossum.alexandra@gmail.com>
2019-04-29 08:58:11 -0500

    commit before class

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -4,21 +4,43 @@ var pictureOne = document.getElementById("pic-1");
 var pictureTwo = document.getElementById("pic-2");
 var pictureThree = document.getElementById("pic-3");
 
+function alertMessage() {
+    var newLine = "\r\n";
+    var message = "Would you like to sign up for our newsletter?";
+    message += newLine;
+    message += "Vil du registrere deg for vårt nyhetsbrev?";
+    alert(message);
+}
+
+alertMessage();
+
 $(document).ready(function () {
     $("#reveal").click(function (e) {
         $('dd').toggleClass('invisible');
     });
     $("h3").click(function () {
         $("li").css({"font-weight": "bold"});
+        $("ul").show();
     });
+
     $("#list-btn").click(function (e) {
         $("li:last-child").css({"background-color": "#FFC038",
                                       "border-radius": "5px"
         });
-        $("li").click(function(e) {
-            // $(this).parent().children().first().css({"color": "dodgerblue"});
-            $("li:first-child").css({"color" : "dodgerblue"});
-        })
+        $("li:first-child").css({"background-color" : "dodgerblue",
+                                        "border-radius": "5px"})
+        // $("li").click(function(e) {
+        //     // $(this).parent().children().first().css({"color": "dodgerblue"});
+        //     $("li:first-child").css({"color" : "dodgerblue"});
+        // })
+    });
+
+    $("#hide").click(function() {
+        $(".newsletter").hide();
+    });
+
+    $(".fade-in").delay(8000).fadeIn();
+
     });
 
 //hillside | lake | hiker
@@ -39,4 +61,3 @@ $(document).ready(function () {
 
         });
     });
-});
